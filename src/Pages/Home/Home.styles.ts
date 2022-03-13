df1c33d2d9bcdb70920666c63c1b6f4e8a2760ab
victor.reis <victor.reis@plexusworldwide.com>
2022-03-13 18:09:22 -0300

    feat: create the home sketch

---
+++ src/Pages/Home/Home.styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+import { Typography } from '../../Components/Typography';
+
+export const HomeTitle = styled(Typography)`
+  margin: 0;
+`;
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 1rem;
+`;
+
+export const HomeVerticalMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+  width: 13rem;
+  position: fixed;
+  padding: 1rem;
+  box-sizing: border-box;
+  height: 100vh;
+
+  border-right-width: 1px;
+  border-right-style: solid;
+  border-right-color: ${({ theme }) => theme.colors.background.default.normal};
+`;
+
+export const HomeFeedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 13rem;
+`;
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,83 @@
+import { PageContainer } from '../../App.styles';
+import { Button } from '../../Components/Button';
+import { FixedBar } from '../../Components/FixedBar';
+import { Post } from '../../Components/Post';
+import { PostCreator } from '../../Components/PostCreator';
+import { QuotePost } from '../../Components/QuotePost';
+import { Repost } from '../../Components/Repost';
+import { TextToggle } from '../../Components/TextToggle';
+import { Typography } from '../../Components/Typography';
+import { TestProps } from '../../Config/Tests/Test.types';
+import { PostType } from '../../Post.types';
+import { QuotePostType } from '../../QuotePost.types';
+import { RepostType } from '../../Repost.types';
+import { UserType } from '../../User.types';
+import { HomeFeedContainer, HomeTitle, HomeVerticalMenu } from './Home.styles';
+
+export const homeDefaults: Required<TestProps> = {
+  testID: 'Home',
+};
+
+export const Home: React.FC = (): JSX.Element => {
+  const userTeste: UserType = {
+    id: 'user123',
+    name: 'Victor Reis',
+    nickname: 'victor.reis',
+    url: '...',
+    thumbnailUrl: 'https://i.pravatar.cc/300',
+    createdAt: new Date(),
+    following: 333,
+    followers: 777,
+  };
+  const post: PostType = {
+    id: 'post123',
+    text: `texto de exemplo\nteste\nteste`,
+    createdAt: new Date(),
+    createdBy: userTeste,
+    reposts: 9,
+    quotPosts: 99,
+  };
+  const repost: RepostType = {
+    id: 'post123',
+    createdAt: new Date(),
+    createdBy: userTeste,
+    originalPost: post,
+  };
+  const quotePost: QuotePostType = {
+    originalPost: post,
+    id: 'post123',
+    text: `texto de exemplo\nteste\nteste`,
+    createdAt: new Date(),
+    createdBy: userTeste,
+    reposts: 9,
+    quotPosts: 99,
+  };
+
+  return (
+    <PageContainer data-testid={homeDefaults.testID}>
+      <HomeVerticalMenu>
+        <HomeTitle variant="h2">Posterr</HomeTitle>
+        <Typography>Home</Typography>
+        <Typography>Profile</Typography>
+        <Button onClick={() => {}}>POST</Button>
+      </HomeVerticalMenu>
+
+      <HomeFeedContainer>
+        <FixedBar justifyContent="space-between">
+          <Typography>Home</Typography>
+          <TextToggle onToggle={() => {}} texts={['ALL', 'FOLLOWING']} />
+        </FixedBar>
+
+        <PostCreator
+          name="Victor Reis"
+          onChange={() => {}}
+          thumbnailUrl="https://i.pravatar.cc/300"
+        />
+
+        <Post {...post} />
+        <Repost {...repost} />
+        <QuotePost {...quotePost} />
+      </HomeFeedContainer>
+    </PageContainer>
+  );
+};
